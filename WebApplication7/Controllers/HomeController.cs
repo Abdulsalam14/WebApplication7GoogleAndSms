b53a0e6df261c854266c3ab4a761c6ea29acc102
Abdulsalam14 <abdullayevabdulsalam@gmail.com>
2024-01-17 19:22:31 +0400

    second

--- WebApplication7/Controllers/HomeController.cs
+++ WebApplication7/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Exchange.WebServices.Auth.Validation;
 using System;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Net;
+using System.Web;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
@@ -12,23 +15,24 @@ namespace WebApplication7.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration = null)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
-
-        public IActionResult SendSms(string number)
+        [HttpPost]
+        public IActionResult SendSms(string phoneNumber,string messageBody)
         {
             var accountSid = "ACcc1934df9b49da6b9284d6c98f4e8a40";
             var authToken = "42df5ae6de0bd3c46ff8132e8c30b116";
             TwilioClient.Init(accountSid, authToken);
 
             var messageOptions = new CreateMessageOptions(
-             new PhoneNumber(number));
+             new PhoneNumber(phoneNumber));
             messageOptions.From = new PhoneNumber("+12024107097");
-            messageOptions.Body = "salam ss";
+            messageOptions.Body = messageBody;
 
             var message = MessageResource.Create(messageOptions);
 
